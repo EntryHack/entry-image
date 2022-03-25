047f0a090c381d316181c3ec67fe32670f7ab908
thoratica <me@tica.fun>
2022-03-25 19:12:12 +0900

    기본 세팅

---
+++ web-ext-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  run: {
+    target: ['chromium'],
+    startUrl: 'https://playentry.org/community/entrystory/list?sort=created&term=all',
+    chromiumBinary: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
+  },
+};
